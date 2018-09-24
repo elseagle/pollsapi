c68dd89e51c5d19e0aecf94539884095b184a1cd
elseagle <dowolebolu@gmail.com>
2018-09-24 16:57:14 +0100

    removed unnecessary details

--- pollsapi/polls/serializers.py
+++ pollsapi/polls/serializers.py
@@ -10,10 +10,6 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
     def create(self, validated_data):
         user = User(email=validated_data['email'], username=validated_data['username'])
-        email = validated_data['email']
-        #username=validated_data['username']
-        # if email and User.objects.filter(email=email).exists():
-        #     return 'Email addresses must be unique.'
         user.set_password(validated_data['password'])
         user.save()
         Token.objects.create(user=user)
